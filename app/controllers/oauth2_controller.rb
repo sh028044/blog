c69414bbbabd935ea23e2df1d5b0de09b4d26cea
YouHua <sh028044@gmail.com>
2014-02-10 22:02:55 +0800

    delete unuseful params

--- app/controllers/oauth2_controller.rb
+++ app/controllers/oauth2_controller.rb
@@ -5,9 +5,6 @@ class Oauth2Controller < ApplicationController
         @@client = OAuth2::Client.new('283272595156318', 'e7ef9daaebec7fe3f9234886bada5fca', :site => params[:login_url], :authorize_url => "/oauth/authorize", :token_url => "/oauth/access_token")
         redirect_to @@client.auth_code.authorize_url(:redirect_uri => 'http://localhost:3000/oauth2')                
       end
-      if params[:access_token]
-        render text: params[:access_token]
-      end
       if params[:code]
          @token=@@client.auth_code.get_token(params[:code], :redirect_uri => 'http://localhost:3000/oauth2')
          redirect_to access_token(@@token_url,@token.to_s)
